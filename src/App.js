d56e2af9f5e51b7992cc19181c588983a1503b67
RUSHIL <69619062+rushilmakvana@users.noreply.github.com>
2022-10-14 19:05:28 +0530

    updated cors and server

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import io from 'socket.io-client'
 import { useState } from 'react'
 import Chat from './Chat';
-const socket = io.connect("https://6349534f8a7a593c8833ffe7--mellow-pithivier-f365d7.netlify.app/");
+const socket = io.connect("https://chat-new-server.herokuapp.com/",transports=["websocket"]);
 
 
 
